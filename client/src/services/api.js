3ea5c196aa622f7fcfedce3f66b6bbbe90a61ffc
amimanye <amimanyeaglago@gmail.com>
2024-09-10 14:52:04 +0000

    Add backend api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://auritech.onrender.com/api";
 
 export const createInvoice = (invoiceData) => {
   return axios.post(`${API_URL}/invoices`, invoiceData);
